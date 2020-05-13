2c07759653ad7dc9ae9df99d2d58b96342bee7c1
Cris Ward <cris@duodesign.co.uk>
2020-05-13 11:04:56 +0100

    added some options

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import decomp from "poly-decomp"
 import { Runner } from "matter-js";
 window.decomp = decomp
 
-var {Engine,Render,World,Bodies,Constraint,MouseConstraint,Composites,Composite} = Matter;
+var {Engine,Render,World,Bodies,Body,Constraint,MouseConstraint,Composites,Composite} = Matter;
 
 // create an engine
 var engine = Engine.create();
@@ -24,11 +24,10 @@ var render = Render.create({
 });
 
 var left = 250
-var slinglength = 40
 
-function createTreb(){
-  
-  engine.constraintIterations = 5
+function createTreb(slinglength,speed,load){
+  console.log("createTreb",slinglength,speed)
+  engine.constraintIterations = 10
   engine.world.gravity.scale = 0.001
   render.options.wireframes = true
   // stage
@@ -47,9 +46,10 @@ function createTreb(){
     {x:-80,y:210},
     {x:-100,y:210}
   ],{ collisionFilter: { group: -1 }, isStatic: true})
-  var arm = Bodies.rectangle(left,400,300,10,{ collisionFilter: { group: -1 }, density:0.1 })
-  var weight = Bodies.rectangle(left+100,400,50,50,{ collisionFilter: { group: -1 }, density:2 })
-  var missile = Bodies.circle(left-175,370,15,{density:0.0002,restitution:0.5,friction:1})
+  var arm = Bodies.rectangle(left-20,440,300,10,{ collisionFilter: { group: -1 }, density:0.1 })
+  var weight = Bodies.rectangle(left+42,310,50,50,{ collisionFilter: { group: -1 }, density:load })
+  var missile = Bodies.circle(left-50,440,15,{density:0.0002,restitution:0.5,friction:1})
+
 
   var armPivot = Constraint.create({
     pointA: { x: left, y: 400 },
@@ -68,7 +68,8 @@ function createTreb(){
     stiffness:1
   })
 
-  var flipper = Bodies.rectangle(left-(200+slinglength),400,10,10,{ density:0.01 })
+  var flipper = Bodies.rectangle(left-(80+slinglength),580,10,10,{ density:0.1 })
+ 
   var flipperPivot = Constraint.create({
     bodyA: flipper,
     pointA: { x: 0, y: 0 },
@@ -79,20 +80,39 @@ function createTreb(){
   })
   World.add(engine.world,[flipper,flipperPivot])
   World.add(engine.world, [arm,weight,armPivot,missile,armWeightJoint,frame]);
+
+  Body.rotate( arm, -Math.PI/2.8 );
   // run the engine
   runner = Engine.run(engine);
   // run the renderer
+  engine.timing.timeScale = speed;
   Render.run(render);
 }
 
-createTreb()
+createTreb(40,1,2)
+
+function reset(){
+
+}
 
 // button
 document.querySelector("#replay").addEventListener("click",function(){
+  let slinglength = +document.querySelector("#slinglength").value
+  let load = +document.querySelector("#load").value
   World.clear(engine.world);
   Engine.clear(engine);
   Render.stop(render);
   Runner.stop(runner)
-  createTreb()
-  console.log("updating 2")
+  createTreb(slinglength,1,load)
 })
+
+// doesn't work
+// document.querySelector("#slowmo").addEventListener("click",function(){
+//   let load = +document.querySelector("#load").value
+//   let slinglength = +document.querySelector("#slinglength").value
+//   World.clear(engine.world);
+//   Engine.clear(engine);
+//   Render.stop(render);
+//   Runner.stop(runner)
+//   createTreb(slinglength,0.1,load)
+// })
